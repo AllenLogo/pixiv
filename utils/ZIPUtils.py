8dc53e7562e8f2b8912a1fdc6c8e2c5ae1bcecf5
allenlogo <ab3500abc>
2016-10-09 14:36:35 +0800

    扫描关注关注

---
+++ utils/ZIPUtils.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+import os
+import zipfile
+
+def zip_dir(dirname,zipfilename):
+    """
+    | ##@函数目的: 压缩指定目录为zip文件
+    | ##@参数说明：dirname为指定的目录，zipfilename为压缩后的zip文件路径
+    | ##@返回值：无
+    | ##@函数逻辑：
+    """
+    filelist = []
+    if os.path.isfile(dirname):
+        filelist.append(dirname)
+    else :
+        for root, dirs, files in os.walk(dirname):
+            for name in files:
+                filelist.append(os.path.join(root, name))
+ 
+    zf = zipfile.ZipFile(zipfilename, "w", zipfile.zlib.DEFLATED)
+    for tar in filelist:
+        arcname = tar[len(dirname):]
+        #print arcname
+        zf.write(tar,arcname)
+    zf.close()
+
+if __name__ == '__main__':
+	import sys
+	zip_dir(sys.argv[1],sys.argv[2])
